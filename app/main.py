08deb4a7e7b729fdaa9e59827aa76cc0fd781375
ab-azmi <azmi.kiva@gmail.com>
2024-04-27 18:38:57 +0700

    add migrations

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, File, UploadFile
+import json
 from app.db import init_db, get_session
 from app.models import Contract, Hypothesis, ContractHypothesis, Label
+from fastapi import UploadFile
 
 app = FastAPI()
 
@@ -19,10 +21,12 @@ async def create_hypothesis(hypothesis: Hypothesis, session = Depends(get_sessio
 async def read_hypotheses(session = Depends(get_session)) -> list[Hypothesis]:
     return session.query(Hypothesis).all()
 
-@app.post("/hypothesis/import")
+@app.post("/hypothesis/import", response_model=list[Hypothesis])
 async def import_hypotheses(hypotheses: list[Hypothesis], session = Depends(get_session)):
     session.add_all(hypotheses)
     session.commit()
+    for hypothesis in hypotheses:
+        session.refresh(hypothesis)
     return hypotheses
 
 @app.post("/contracts")
@@ -35,6 +39,19 @@ async def create_contract(contract: Contract, session = Depends(get_session)):
 @app.get("/contracts")
 async def read_contracts(session = Depends(get_session)) -> list[Contract]:
     return session.query(Contract).all()
+
+@app.post("/contracts/import")
+async def import_contracts(file: UploadFile = File(), session = Depends(get_session)):
+    contracts = await file.read()
+    # Process the contracts data
+    contracts = json.loads(contracts)
+    contracts = [Contract(
+        content=contract['premise'],
+    ) for contract in contracts]
+
+    session.add_all(contracts)
+    session.commit()
+    return 'Contracts imported successfully'
 
 #analyze take contract_id and hypothesis_id and return the label
 @app.post("/analyze")
